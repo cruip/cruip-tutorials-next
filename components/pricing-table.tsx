ed3af7c9b5e80e12f4daca3285cb7c839f53dccc
pasqualevitiello <pasqualevitiello@gmail.com>
2023-05-15 12:04:49 +0200

    change variable name

--- components/pricing-table.tsx
+++ components/pricing-table.tsx
@@ -54,7 +54,7 @@ function PricingTab(props: PricingTabProps) {
 }
 
 export default function PricingTable() {
-  const [yearly, setYearly] = useState<boolean>(true)
+  const [isAnnual, setIsAnnual] = useState<boolean>(true)
 
   return (
     <div>
@@ -63,10 +63,10 @@ export default function PricingTable() {
       <div className="flex justify-center max-w-[14rem] m-auto mb-8 lg:mb-16">
         <div className="relative flex w-full p-1 bg-white dark:bg-slate-900 rounded-full">
           <span className="absolute inset-0 m-1 pointer-events-none" aria-hidden="true">
-            <span className={`absolute inset-0 w-1/2 bg-indigo-500 rounded-full shadow-sm shadow-indigo-950/10 transform transition-transform duration-150 ease-in-out ${yearly ? 'translate-x-0' : 'translate-x-full'}`}></span>
+            <span className={`absolute inset-0 w-1/2 bg-indigo-500 rounded-full shadow-sm shadow-indigo-950/10 transform transition-transform duration-150 ease-in-out ${isAnnual ? 'translate-x-0' : 'translate-x-full'}`}></span>
           </span>
-          <button className={`relative flex-1 text-sm font-medium h-8 rounded-full focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-600 transition-colors duration-150 ease-in-out ${yearly ? 'text-white' : 'text-slate-500 dark:text-slate-400'}`} onClick={() => setYearly(true)}>Yearly <span className={`${yearly ? 'text-indigo-200' : 'text-slate-400 dark:text-slate-500'}`}>-20%</span></button>
-          <button className={`relative flex-1 text-sm font-medium h-8 rounded-full focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-600 transition-colors duration-150 ease-in-out ${yearly ? 'text-slate-500 dark:text-slate-400' : 'text-white'}`} onClick={() => setYearly(false)}>Monthly</button>
+          <button className={`relative flex-1 text-sm font-medium h-8 rounded-full focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-600 transition-colors duration-150 ease-in-out ${isAnnual ? 'text-white' : 'text-slate-500 dark:text-slate-400'}`} onClick={() => setIsAnnual(true)}>Yearly <span className={`${isAnnual ? 'text-indigo-200' : 'text-slate-400 dark:text-slate-500'}`}>-20%</span></button>
+          <button className={`relative flex-1 text-sm font-medium h-8 rounded-full focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-600 transition-colors duration-150 ease-in-out ${isAnnual ? 'text-slate-500 dark:text-slate-400' : 'text-white'}`} onClick={() => setIsAnnual(false)}>Monthly</button>
         </div>
       </div>
 
@@ -74,7 +74,7 @@ export default function PricingTable() {
 
         {/* Pricing tab 1 */}
         <PricingTab
-          yearly={yearly}
+          yearly={isAnnual}
           planName="Essential"
           price={{ yearly: 29, monthly: 35 }}
           planDescription="There are many variations available, but the majority have suffered."
@@ -87,7 +87,7 @@ export default function PricingTable() {
 
         {/* Pricing tab 2 */}
         <PricingTab
-          yearly={yearly}
+          yearly={isAnnual}
           popular={true}
           planName="Perform"
           price={{ yearly: 49, monthly: 55 }}
@@ -102,7 +102,7 @@ export default function PricingTable() {
 
         {/* Pricing tab 3 */}
         <PricingTab
-          yearly={yearly}
+          yearly={isAnnual}
           planName="Enterprise"
           price={{ yearly: 79, monthly: 85 }}
           planDescription="There are many variations available, but the majority have suffered."
