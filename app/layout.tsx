50c6cd73a729107428b11c90cf8aedf8f64187e6
pasqualevitiello <pasqualevitiello@gmail.com>
2023-05-24 14:28:25 +0200

    spotlight tutorial

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} font-inter antialiased`}>
+      <body className={`${inter.variable} relative font-inter antialiased`}>
         {children}
         <footer className="absolute left-6 right-6 md:left-12 md:right-auto bottom-4 md:bottom-8 text-center md:text-left">
           <a className="text-xs text-slate-500 hover:underline" href="https://cruip.com">&copy;Cruip - Tailwind CSS templates</a>
