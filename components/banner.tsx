ee9fc69477f9d65e58220f2d351f53c2ef9dfc3f
pasqualevitiello <pasqualevitiello@gmail.com>
2023-06-13 12:27:47 +0200

    mc

--- components/banner.tsx
+++ components/banner.tsx
@@ -30,7 +30,7 @@ export default function Banner({
                 </svg>
               </a>
             </div>
-            <button className="text-slate-500 hover:text-slate-400 pl-2 ml-3 border-l border-gray-700" onClick={() => setBannerOpen(false)}>
+            <button className="text-slate-500 hover:text-slate-400 pl-2 ml-3 border-l border-slate-700" onClick={() => setBannerOpen(false)}>
               <span className="sr-only">Close</span>
               <svg className="w-4 h-4 shrink-0 fill-current" viewBox="0 0 16 16">
                 <path d="M12.72 3.293a1 1 0 00-1.415 0L8.012 6.586 4.72 3.293a1 1 0 00-1.414 1.414L6.598 8l-3.293 3.293a1 1 0 101.414 1.414l3.293-3.293 3.293 3.293a1 1 0 001.414-1.414L9.426 8l3.293-3.293a1 1 0 000-1.414z" />
