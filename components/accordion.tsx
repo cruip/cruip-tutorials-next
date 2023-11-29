3013c2bee28f4d7c364985163b781fa52d928889
pasqualevitiello <pasqualevitiello@gmail.com>
2023-11-29 11:35:59 +0100

    mc

--- components/accordion.tsx
+++ components/accordion.tsx
@@ -33,7 +33,7 @@ export default function Accordion({
         >
           <span>{title}</span>
           <svg className="fill-indigo-500 shrink-0 ml-8" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
-            <rect y="7" width="16" height="2" rx="1" className={`ttransform origin-center transition duration-200 ease-out ${accordionOpen && '!rotate-180'}`} />
+            <rect y="7" width="16" height="2" rx="1" className={`transform origin-center transition duration-200 ease-out ${accordionOpen && '!rotate-180'}`} />
             <rect y="7" width="16" height="2" rx="1" className={`transform origin-center rotate-90 transition duration-200 ease-out ${accordionOpen && '!rotate-180'}`} />
           </svg>           
         </button>        
